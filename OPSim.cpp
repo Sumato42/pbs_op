e0a2ff8526ea07dabb9ab3674104bd74cb42c339
Beyzanur Çoban <beyzanur.cobann@gmail.com>
2023-11-21 15:13:28 +0100

    added basic simulation meshes

--- OPSim.cpp
+++ OPSim.cpp
@@ -10,11 +10,11 @@ bool OPSim::advance() {
 }
 
 void OPSim::loadOBJ(){
-        std::string path = "Frank/Frank.obj";
+        std::string path = "circular.obj";
         m_objects.clear();
         m_objects.push_back(RigidObject(path));
         p_obj = &m_objects.back();
+        p_obj->setScale(0.1);
         //igl::readOBJ(path, m_renderV, m_renderTC, m_renderN, m_renderF, m_renderFTC, m_renderFN);
 }
-
 
