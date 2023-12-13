f1f08d3f9db51c5ae93aa3b1f912e43e31b3c108
Beyzanur Çoban <beyzanur.cobann@gmail.com>
2023-12-13 14:54:59 +0100

    anim and sim particles

--- PBS.cs
+++ PBS.cs
@@ -33,7 +33,8 @@ public class Hash
 
     public List<int> QueryIds;
 
-    public Hash(double spacing, int maxNumObjects){
+    public Hash(double spacing, int maxNumObjects)
+    {
         this.Spacing = spacing;
         this.TableSize = 2 * maxNumObjects;
         this.CellStart = Enumerable.Repeat(0, this.TableSize + 1).ToList();
@@ -42,32 +43,37 @@ public class Hash
         this.MaxNumObjects = maxNumObjects;
     }
 
-    public void create(List<Particle> particles){
+    public void create(List<Particle> particles)
+    {
         int numObjects = Mathf.Min(particles.Count, this.CellEntries.Count);
         this.CellStart = Enumerable.Repeat(0, this.TableSize + 1).ToList();
         this.CellEntries = Enumerable.Repeat(0, this.MaxNumObjects).ToList();
 
-        for (int i = 0; i < numObjects; i++){
+        for (int i = 0; i < numObjects; i++)
+        {
             int h = hashPos(particles[i].Position);
             this.CellStart[h]++;
         }
-        
+
         int start = 0;
-        for(int i = 0; i < this.TableSize; i++){
+        for (int i = 0; i < this.TableSize; i++)
+        {
             start += this.CellStart[i];
             this.CellStart[i] = start;
         }
 
         this.CellStart[this.TableSize] = start;
 
-        for(int i = 0; i < numObjects; i++){
+        for (int i = 0; i < numObjects; i++)
+        {
             int h = hashPos(particles[i].Position);
             this.CellStart[h]--;
             this.CellEntries[this.CellStart[h]] = i;
         }
     }
 
-    public void query(Particle particle, double maxDist){
+    public void query(Particle particle, double maxDist)
+    {
         Vector3 pos = particle.Position;
         int x0 = intCoord(pos.x - maxDist);
         int y0 = intCoord(pos.y - maxDist);
@@ -79,33 +85,40 @@ public class Hash
 
         //this.QuerySize = 0; 
         this.QueryIds = new List<int>();
-        
-        for(int xi = x0; xi <= x1; xi++){
-            for(int yi = y0; yi <= y1; yi++){
-                for(int zi = z0; zi <= z1; zi++){
+
+        for (int xi = x0; xi <= x1; xi++)
+        {
+            for (int yi = y0; yi <= y1; yi++)
+            {
+                for (int zi = z0; zi <= z1; zi++)
+                {
                     int h = hashCoords(xi, yi, zi);
                     int start = this.CellStart[h];
                     int end = this.CellStart[h + 1];
-                    for(int i = start; i < end; i++){
+                    for (int i = start; i < end; i++)
+                    {
                         this.QueryIds.Add(this.CellEntries[i]);
                         //QuerySize++;
                     }
                 }
             }
         }
-        
+
     }
 
-    public int hashCoords(int xi, int yi, int zi){
+    public int hashCoords(int xi, int yi, int zi)
+    {
         int h = (xi * 92837111) ^ (yi * 689287499) ^ (zi * 283923481);
         return Mathf.Abs(h) % this.TableSize;
     }
 
-    public int intCoord(double coord){
+    public int intCoord(double coord)
+    {
         return (int)(Mathf.Floor((float)(coord / this.Spacing)));
     }
 
-    public int hashPos(Vector3 pos){
+    public int hashPos(Vector3 pos)
+    {
         return hashCoords(intCoord(pos.x), intCoord(pos.y), intCoord(pos.z));
     }
 }
@@ -115,9 +128,12 @@ public class PBS : MonoBehaviour
     [SerializeField]
     GameObject[] M_Objects = null;
 
-    List<Particle> M_Particles = new();
+    List<Particle> Particles_Anim = new();
+    List<Particle> Particles_Sim= new();
 
     int[,] adjacencyMatrix;
+
+    List<Mesh> meshes = new();
 
     public Vector3 Gravitation = new Vector3(0, -9.81f, 0);
     public int Num_substep = 1;
@@ -130,71 +146,82 @@ public class PBS : MonoBehaviour
     void Start()
     {
         Debug.Log("Start!");
-        if (M_Objects.Length == 0) {
+        if (M_Objects.Length == 0)
+        {
             return;
         }
 
         // setting the parameters for the simulation
         M_dt /= Num_substep;
-        
+
         // generate and store Particles
-        foreach(GameObject m in M_Objects)
-        {
-            Transform beta_surface = m.transform.Find("Beta_Surface");
-            SkinnedMeshRenderer m_render = beta_surface.GetComponent<SkinnedMeshRenderer>();
-            Mesh beta_mesh = m_render.sharedMesh;
-            
-            assingParticles(beta_mesh);
-        }
-        Debug.Log("Particles assigned!");
-
-        // Assign the adjacency matrix after particles are assigned
-        adjacencyMatrix = new int[M_Particles.Count, M_Particles.Count];
-        InitializeMatrixToZero(adjacencyMatrix);
-
         foreach (GameObject m in M_Objects)
         {
             Transform beta_surface = m.transform.Find("Beta_Surface");
             SkinnedMeshRenderer m_render = beta_surface.GetComponent<SkinnedMeshRenderer>();
             Mesh beta_mesh = m_render.sharedMesh;
+            meshes.Add(beta_mesh);
 
-            updateAdjacencyList(beta_mesh);
+            assingParticles(beta_mesh);
+        }
+        Debug.Log("Particles assigned!");
+
+        // Assign the adjacency matrix after particles are assigned
+        adjacencyMatrix = new int[Particles_Sim.Count, Particles_Sim.Count];
+        InitializeMatrixToZero(adjacencyMatrix);
+
+        foreach (Mesh m in meshes)
+        {
+            updateAdjacencyList(m);
         }
 
         Debug.Log("Adjacency list assigned!");
 
         // create a hash
-        ParticleHash = new Hash(Particle_radius, M_Particles.Count);
-        Debug.Log(M_Particles.Count);
-        
+        ParticleHash = new Hash(Particle_radius, Particles_Sim.Count);
+        Debug.Log(Particles_Sim.Count);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Num_substep; i++){
+        // Update animated particle positions
+        foreach (Mesh m in meshes)
+        {
+            updateParticles(m);
+        }
+
+        // Predict simulation particles position
+        for (int i = 0; i < Num_substep; i++)
+        {
             // update Hash
-            ParticleHash.create(M_Particles);
+            ParticleHash.create(Particles_Sim);
             // prediction 
-            for(int j = 0; j < M_Particles.Count; j++){
-                M_Particles[j].Position = M_Particles[j].OldPosition + M_Particles[j].Velocity * M_dt;
+            for (int j = 0; j < Particles_Sim.Count; j++)
+            {
+                Particles_Sim[j].Position = Particles_Sim[j].OldPosition + Particles_Sim[j].Velocity * M_dt;
             }
             // solve constraints
-            for(int j = 0; j < M_Particles.Count; j++){
-                groundConstraint(this.M_Particles[j].Position);
+            for (int j = 0; j < Particles_Sim.Count; j++)
+            {
+                groundConstraint(this.Particles_Anim[j].Position);
             }
             collisionConstraint();
             // update meshes 
         }
-        
+
     }
 
-    void InitializeMatrixToZero(int[,] matrix) {
+    void InitializeMatrixToZero(int[,] matrix)
+    {
         int rows = matrix.GetLength(0);
         int columns = matrix.GetLength(1);
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
                 matrix[i, j] = 0;
             }
         }
@@ -203,16 +230,25 @@ public class PBS : MonoBehaviour
     void assingParticles(Mesh mesh)
     {
         Vector3[] vertices = mesh.vertices;
-        
-        for(int i = 0; i < vertices.Length; i++)
+
+        for (int i = 0; i < vertices.Length; i++)
         {
             Particle part = new Particle(vertices[i]);
             part.IsSimulationParticle = false;
-            M_Particles.Add(part);
+            Particles_Anim.Add(part);
             /* GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.transform.position = vertices[i];
             sphere.transform.localScale= Vector3.one*0.01f;
             */
+        }
+    }
+
+    private void updateParticles(Mesh mesh)
+    {
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Particles_Anim[i].Position = vertices[i];
         }
     }
 
@@ -227,13 +263,13 @@ public class PBS : MonoBehaviour
             int index2 = triangles[i * 3 + 1];
             int index3 = triangles[i * 3 + 2];
 
-            Particle v1 = M_Particles[index1];
-            Particle v2 = M_Particles[index2];
-            Particle v3 = M_Particles[index3];
+            Particle v1 = Particles_Anim[index1];
+            Particle v2 = Particles_Anim[index2];
+            Particle v3 = Particles_Anim[index3];
 
-            int i1 = M_Particles.IndexOf(v1);
-            int i2 = M_Particles.IndexOf(v2);
-            int i3 = M_Particles.IndexOf(v3);
+            int i1 = Particles_Anim.IndexOf(v1);
+            int i2 = Particles_Anim.IndexOf(v2);
+            int i3 = Particles_Anim.IndexOf(v3);
 
             if (i1 != -1 && i2 != -1) addEdge(i1, i2);
             if (i2 != -1 && i3 != -1) addEdge(i2, i3);
@@ -243,7 +279,7 @@ public class PBS : MonoBehaviour
 
     void addEdge(int v1, int v2)
     {
-        if (v1 >= 0 && v1 < M_Particles.Count && v2 >= 0 && v2 < M_Particles.Count)
+        if (v1 >= 0 && v1 < Particles_Anim.Count && v2 >= 0 && v2 < Particles_Anim.Count)
         {
             adjacencyMatrix[v1, v2] = 1;
             adjacencyMatrix[v2, v1] = 1;
@@ -252,8 +288,8 @@ public class PBS : MonoBehaviour
 
 
     void groundConstraint(Vector3 pos)
-    {   
-        if(pos.y >= 0)
+    {
+        if (pos.y >= 0)
             return;
         float C = pos.y;
         Vector3 dC = new Vector3(0, 1, 0);
@@ -269,33 +305,32 @@ public class PBS : MonoBehaviour
     void collisionConstraint()
     {
         // check for all particles for collisions with the help of the ParticleHash
-        for (int i = 0; i < M_Particles.Count; i++){
-            if (M_Particles[i].IsSimulationParticle)
+        for (int i = 0; i < Particles_Sim.Count; i++)
+        {
+            ParticleHash.query(Particles_Sim[i], 2 * Particle_radius);
+
+            foreach (int j in ParticleHash.QueryIds)
             {
-                ParticleHash.query(M_Particles[i], 2 * Particle_radius);
-
-                foreach (int j in ParticleHash.QueryIds)
+                Vector3 pi = Particles_Sim[i].Position;
+                Vector3 pj = Particles_Sim[j].Position;
+                Vector3 normal = pi - pj;
+                float dist = Mathf.Sqrt(normal.sqrMagnitude);
+                if (dist > 0 && dist < 2 * Particle_radius)
                 {
-                    Vector3 pi = M_Particles[i].Position;
-                    Vector3 pj = M_Particles[j].Position;
-                    Vector3 normal = pi - pj;
-                    float dist = Mathf.Sqrt(normal.sqrMagnitude);
-                    if (dist > 0 && dist < 2 * Particle_radius)
-                    {
-                        float C = dist - 2 * Particle_radius;
-                        Vector3 dC1 = normal / dist * C;
-                        Vector3 dC2 = -normal / dist * C;
-                        M_Particles[i].Position += dC1;
-                        M_Particles[j].Position += dC2;
-                        Vector3.Cross(dC1, dC2);
-                        float v1 = Vector3.Dot(M_Particles[i].Velocity, normal);
-                        float v2 = Vector3.Dot(M_Particles[j].Velocity, normal);
+                    float C = dist - 2 * Particle_radius;
+                    Vector3 dC1 = normal / dist * C;
+                    Vector3 dC2 = -normal / dist * C;
+                    Particles_Sim[i].Position += dC1;
+                    Particles_Sim[j].Position += dC2;
+                    Vector3.Cross(dC1, dC2);
+                    float v1 = Vector3.Dot(Particles_Sim[i].Velocity, normal);
+                    float v2 = Vector3.Dot(Particles_Sim[j].Velocity, normal);
 
-                        M_Particles[i].Velocity += normal * (v2 - v1);
-                        M_Particles[j].Velocity += normal * (v1 - v2);
-                    }
+                    Particles_Sim[i].Velocity += normal * (v2 - v1);
+                    Particles_Sim[j].Velocity += normal * (v1 - v2);
                 }
             }
+         
         }
     }
 
